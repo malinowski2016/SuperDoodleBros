df88033aa7e4eb26e9b783edab38d1c2d9389324
malinowski2016 <marcmalinowski2016@u.northwestern.edu>
2016-02-15 18:20:20 -0600

    Added Basic/Tracker Enemies

---
+++ SuperDoodleBros/Assets/Scripts/DisplayPlayerHealth.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DisplayPlayerHealth : MonoBehaviour {
+
+    public GameObject player;
+    public Slider slider;
+
+	// Use this for initialization
+	void Start () {
+        slider = GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        slider.value = player.GetComponent<PlayerHealth>().currentHealth;
+	}
+}
---
+++ SuperDoodleBros/Assets/Scripts/EnemyHealthBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealthBar : MonoBehaviour {
+
+    public Transform target;
+
+    // Use this for initialization
+    void Start(){
+        target = GameObject.FindGameObjectWithTag("Enemy").transform;
+    }
+
+    void Update(){
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(target.position);
+        Debug.Log("target is " + screenPos.x + " pixels from the left");
+    }
+}
