b20f7aebd1476cc0db150f08a59cece9a98fe7b6
curtiswiese <curtiswiese2016@u.northwestern.edu>
2016-02-17 12:23:56 -0600

    Added fireball
    Added fireball weapon
    should be able to set up collision immediately by tagging enemy with
    "Enemy"

---
+++ SuperDoodleBros/Assets/FireballManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballManager : MonoBehaviour {
+
+	private Vector3 velocity; 
+	public float fireball_velo = 1f;
+
+	// Use this for initialization
+	void Start () {
+
+		if (GameObject.FindGameObjectWithTag ("Player").GetComponent<DoodleBroScript> ().dir_facing == 1) {
+			velocity = new Vector3 (fireball_velo, 0f, 0f);
+		} else {
+			velocity = new Vector3 (-1f * fireball_velo,  0f, 0f);
+
+		}
+
+		this.gameObject.GetComponent<Rigidbody2D> ().AddForce (velocity, ForceMode2D.Force);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.gameObject.transform.position.y < (Camera.main.transform.position.y - Camera.main.orthographicSize - 1)) {
+			Destroy (this.gameObject);
+		}
+	}
+
+
+	void OnCollisionEnter2D(Collision2D col){
+		// right now just checks tag and destroys itself
+		// eventually need to do damage
+		if (col.gameObject.tag == "Enemy") {
+			Debug.Log (string.Format ("Hit!"));
+			Destroy (this.gameObject);
+		}
+	}
+
+}
