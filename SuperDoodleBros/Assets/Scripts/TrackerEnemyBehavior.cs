b667a467e6c8a47629fe4774859cf4b92d423ba0
malinowski2016 <marcmalinowski2016@u.northwestern.edu>
2016-02-22 17:39:01 -0600

    Fixed tracker movement

--- SuperDoodleBros/Assets/Scripts/TrackerEnemyBehavior.cs
+++ SuperDoodleBros/Assets/Scripts/TrackerEnemyBehavior.cs
@@ -11,6 +11,7 @@ public class TrackerEnemyBehavior : MonoBehaviour {
     public int MoveSpeed = 5;
  	public int MaxDist = 10;
  	public int MinDist = 5;
+	Vector3 direction;
 
 	// Health
 	public float maxHealth = 100f;
@@ -29,13 +30,15 @@ public class TrackerEnemyBehavior : MonoBehaviour {
         target = GameObject.FindWithTag("Player");
         targetTrans = target.transform;
         playerHealth = target.GetComponent<PlayerHealth>();
+
+		//Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>
     }
  
  	void Update (){
-    	transform.LookAt(targetTrans);
+		direction = targetTrans.position - transform.position;
 		//if (Vector3.Distance(transform.position,targetTrans.position) <= range) {
 			//Debug.Log("IN RANGE");
-			transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+		transform.position += direction.normalized * MoveSpeed * Time.deltaTime;
 		//}
 
 		// Destroys tracker after certain amount of time
@@ -58,17 +61,19 @@ public class TrackerEnemyBehavior : MonoBehaviour {
 		return enemy;
 	}
 
-/* TURNED OFF COLLIDER TO AVOID COLLISIONS W/ PLATFORMS
-    void OnCollisionEnter2D(Collider2D other)
+/*TURNED OFF COLLIDER TO AVOID COLLISIONS W/ PLATFORMS
+	void OnCollisionEnter2D(Collision2D other)
     {
         // If the entering collider is the player...
-        if (other.gameObject == target)
-        {
-            if (playerHealth.currentHealth > 0)
-            {
-                playerHealth.TakeDamage(attackDmg);
-            }
-        }
+		if (other.gameObject == target) {
+			if (playerHealth.currentHealth > 0) {
+				playerHealth.TakeDamage (attackDmg);
+			}
+		} else if (other.gameObject.tag == "projectile") {
+			Destroy (gameObject);
+		} else {
+			Physics2D.IgnoreCollision (other.collider, collider); //gameObject.GetComponent<Collider2D>);
+		}
     }
 
     void OnCollisionExit()
@@ -76,5 +81,5 @@ public class TrackerEnemyBehavior : MonoBehaviour {
         // If the exiting collider is the player...
         //if (other.gameObject == target) { }
     }
-*/
+    */
 }
