c9dff1b872c5542d0d6899fbf50052dae8e6aa57
malinowski2016 <marcmalinowski2016@u.northwestern.edu>
2016-02-29 20:47:30 -0600

    Added Start/End Screens

---
+++ SuperDoodleBros/Assets/LoadLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+
+	public void loadGame () {
+		SceneManager.LoadScene ("Demo4");
+	}
+
+	public void loadDirections() {
+		SceneManager.LoadScene ("Direction Screen");
+	}
+}
--- SuperDoodleBros/Assets/Scripts/DoodleBroScript.cs
+++ SuperDoodleBros/Assets/Scripts/DoodleBroScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class DoodleBroScript : MonoBehaviour {
 
